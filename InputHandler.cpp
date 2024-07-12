839ad1ce6461b5a3483f0b42039c3046a6f6f421
AXE25 <104097379+Kruno-cloud@users.noreply.github.com>
2024-07-12 09:33:49 +0200

    Update InputHandler.cpp

--- InputHandler.cpp
+++ InputHandler.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include "InputHandler.h"
-
-void InputHandler::	GetInput(double& a, char& oper, double& b) 
+#include <cstdlib>
+#include <sstream>
+void InputHandler::GetInput(double& a, char& oper, double& b)
 {
-	std::cout << "Unesite operaciju (npr. 5+5): ";
-	std::cin >> a >> oper >> b;
+    std::string input;
+    std::cout << "Unesite operaciju (npr. 5+5), x za izlaz: ";
+    std::cin >> input;
+
+    // Provjera je li korisnik unio 'x' za izlaz
+    if (input == "x") {
+        std::cout << "Izlaz iz programa." << std::endl;
+        exit(0); // Izlazak iz programa
+    }
+
+    // Pretvaranje unosa u string stream radi jednostavnijeg pariranja
+    std::stringstream ss(input);
+    ss >> a >> oper >> b;
+
 }
+
 
